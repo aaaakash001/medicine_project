47313f0399263db514d524d9e17a6e79ede577a3
Chaitanya Bharat <chaitanyashinge@cse.iitb.ac.in>
2023-11-14 21:10:32 +0530

    Automate database generation and data population

---
+++ database/queries/init.sql
@@ -0,0 +1,41 @@
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
+        CREATE TYPE role_enum AS ENUM ('doctor', 'patient', 'admin');
+        CREATE TABLE users (
+            username VARCHAR PRIMARY KEY,
+            name VARCHAR,
+            role role_enum,
+            password VARCHAR
+        );
+        INSERT INTO users(username, name, role, password) VALUES ('admin', 'admin', 'admin', 'admin');
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'medicine') THEN
+        CREATE TEMPORARY TABLE temp_medicine (
+            name VARCHAR,
+            brand VARCHAR,
+            type VARCHAR,
+            composition VARCHAR,
+            price REAL,
+            prescription VARCHAR
+        );
+
+        CREATE TABLE medicine (
+            name VARCHAR,
+            brand VARCHAR,
+            type VARCHAR,
+            composition VARCHAR,
+            price REAL,
+            prescription VARCHAR,
+            PRIMARY KEY (name, type, price)
+        );
+        
+        COPY temp_medicine(name, brand, type, composition, price, prescription) FROM '/docker-entrypoint-initdb.d/all_medicines.csv' DELIMITER ',' CSV HEADER;
+
+        INSERT INTO medicine (name, brand, type, composition, price, prescription)
+        SELECT DISTINCT * FROM temp_medicine
+        ON CONFLICT (name, type, price) DO NOTHING;
+        
+        DROP TABLE IF EXISTS temp_medicine;
+    END IF;
+END $$;
