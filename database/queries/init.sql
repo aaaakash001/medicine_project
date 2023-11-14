87e13dcb17e0eacee3dc9ffbdadf4bae48d7a581
Chaitanya Bharat <chaitanyashinge@cse.iitb.ac.in>
2023-11-15 01:40:18 +0530

    [FIX] change data type of prescription to boolean

--- database/queries/init.sql
+++ database/queries/init.sql
@@ -26,15 +26,24 @@ BEGIN
             type VARCHAR,
             composition VARCHAR,
             price REAL,
-            prescription VARCHAR NOT NULL,
+            prescription BOOLEAN DEFAULT false,
             PRIMARY KEY (name, brand, type, composition)
         );
         
         COPY temp_medicine(name, brand, type, composition, price, prescription) FROM '/docker-entrypoint-initdb.d/all_medicines.csv' DELIMITER ',' CSV HEADER;
 
         INSERT INTO medicine (name, brand, type, composition, price, prescription)
-        SELECT DISTINCT * FROM temp_medicine
-        ON CONFLICT (name, type, price) DO NOTHING;
+        SELECT DISTINCT
+            temp_medicine.name,
+            temp_medicine.brand,
+            temp_medicine.type,
+            temp_medicine.composition,
+            temp_medicine.price,
+            CASE
+                WHEN temp_medicine.prescription = 'Prescription Required' THEN true
+                ELSE false
+            END AS prescription
+        FROM temp_medicine ON CONFLICT (name, brand, type, composition) DO NOTHING;
         
         DROP TABLE IF EXISTS temp_medicine;
     END IF;
